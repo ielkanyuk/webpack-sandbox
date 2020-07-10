1e999ec4533973640afa4adc0920b9a478c76e3c
Igor Elkanyuk <I.Elkanyuk@modulbank.ru>
2020-07-10 22:37:24 +0500

    Css loaders

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import * as $ from 'jquery';
 import Post from '@models/Post';
 import './styles/styles.css';
+import './styles/less.less';
+import './styles/scss.scss';
 import json from './assets/json';
 import xml from './assets/data.xml';
 import csv from './assets/data.csv';
