ad671c8a9f6ffa41dc546d42cbd9fa5186ec04b0
Igor Elkanyuk <I.Elkanyuk@modulbank.ru>
2020-07-10 16:32:45 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import * as $ from 'jquery';
+import Post from '@models/Post';
+import './styles/styles.css';
+import json from './assets/json';
+import xml from './assets/data.xml';
+import csv from './assets/data.csv';
+import WebpackLogo from './assets/webpack-logo';
+
+const post = new Post('Webpack post title', WebpackLogo);
+
+$('pre').addClass('code').html(post.toString());
+
+console.log('Post to string: ', post.toString());
+console.log('JSON: ', json);
+console.log('XML: ', xml);
+console.log('CSV: ', csv);
